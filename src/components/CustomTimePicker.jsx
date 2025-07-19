0af82f5d9dc79570c9c786093f5c8a74e18ee79c
Nazım Ünlü <nazimunlu@Nazms-MacBook-Air.local>
2025-07-19 17:39:21 +0300

    fix: Import useEffect in CustomTimePicker

--- src/components/CustomTimePicker.jsx
+++ src/components/CustomTimePicker.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useClickOutside } from '../hooks/useClickOutside';
 import { FormInput } from './Form';
 import { Icon, ICONS } from './Icons';
