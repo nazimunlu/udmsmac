c35dd7548efe9898c90a68bb603db0c8ba06c3c0
Nazım Ünlü <nazimunlu@Nazms-MacBook-Air.local>
2025-07-19 17:44:20 +0300

    fix: Move tutoring student checkbox to the right in StudentFormModal

--- src/components/StudentFormModal.jsx
+++ src/components/StudentFormModal.jsx
@@ -229,7 +229,7 @@ const StudentFormModal = ({ isOpen, onClose, studentToEdit }) => {
                         <CustomDatePicker label="Enrollment Date" name="enrollmentDate" value={formData.enrollmentDate} onChange={handleChange} />
                     </div>
                      <div className="sm:col-span-3"><CustomDatePicker label="Birth Date (Optional)" name="birthDate" value={formData.birthDate} onChange={handleChange} /></div>
-                    <div className="sm:col-span-3 col-start-4 flex items-center justify-end pt-5">
+                    <div className="sm:col-span-6 flex items-center justify-end pt-5">
                         <label className="relative inline-flex items-center cursor-pointer">
                             <input type="checkbox" name="isTutoring" checked={formData.isTutoring} onChange={handleChange} className="sr-only peer" />
                             <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
