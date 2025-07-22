43b3f4429e8b91f4c26f913c7ea87e885ec203fa
Nazım Ünlü <nazimunlu@Nazms-MacBook-Air.local>
2025-07-22 18:06:09 +0300

    fix: Update drop_tables.sql to include all tables

--- drop_tables.sql
+++ drop_tables.sql
@@ -1,8 +1,13 @@
+-- Drop tables in an order that respects foreign key constraints, or use CASCADE
+DROP TABLE IF EXISTS public.lessons CASCADE;
+DROP TABLE IF EXISTS public.transactions CASCADE;
+DROP TABLE IF EXISTS public.students CASCADE;
+DROP TABLE IF EXISTS public.todos CASCADE;
+DROP TABLE IF EXISTS public.groups CASCADE;
+DROP TABLE IF EXISTS public.documents CASCADE;
+DROP TABLE IF EXISTS public.events CASCADE;
+DROP TABLE IF EXISTS public.settings CASCADE;
 
-DROP TABLE IF EXISTS payments CASCADE;
-DROP TABLE IF EXISTS expenses CASCADE;
-DROP TABLE IF EXISTS students CASCADE;
-DROP TABLE IF EXISTS groups CASCADE;
-
+-- Also drop the functions I created earlier, just in case they were run
 DROP FUNCTION IF EXISTS get_student_group(UUID);
 DROP FUNCTION IF EXISTS get_student_payments(UUID);
